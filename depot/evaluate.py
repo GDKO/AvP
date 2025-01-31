55deccc7943646d263e0b5da4f40395ff16a52cb
Georgios Koutsovoulos <gdkoutsovoulos@gmail.com>
2025-01-31 17:22:03 +0200

    v1.0.10
    Improved user feedback and cleaner code for evaluate.

--- depot/evaluate.py
+++ depot/evaluate.py
@@ -14,15 +14,16 @@
 
 import sys
 import os
-import yaml
+from multiprocessing import Pool
 import subprocess
+import yaml
 
 from docopt import docopt
 from Bio import SeqIO
-from multiprocessing import Pool
 from depot.PetIO import check_programs, open_file, get_outdir, progress, check_indir, run_fasttree, run_iqtree, fix_iqtree, get_num_jobs
 
 def main():
+    """Evaluates alternative topologies"""
 
     # =============== #
     # 	PARAMETERS    #
@@ -30,14 +31,13 @@ def main():
 
     args = docopt(__doc__)
 
-    input_dir = check_indir(args['--input'])
+    check_indir(args['--input'])
     tree_results_file = args['--tree_results']
     output_dir = get_outdir(args['--output'])
     config_yaml = args['--config_file']
 
-    stream = open(config_yaml, 'r')
-    config_opts = yaml.safe_load(stream)
-    stream.close()
+    with open(config_yaml, 'r', encoding = "UTF-8") as stream:
+        config_opts = yaml.safe_load(stream)
 
     data_type = config_opts["data_type"]
     threads = config_opts["max_threads"]
@@ -46,9 +46,6 @@ def main():
     iq_threads = config_opts["iq_threads"]
     iqmodel = config_opts["iqmodel"]
     ufbootstrap = config_opts["ufbootstrap"]
-
-    fasttree_model = ""
-    fastml_iqmodel = ""
 
     if data_type == "AA":
         fasttree_model = "-gamma -lg"
@@ -66,28 +63,18 @@ def main():
 
     print ("[+] Setting up")
 
-    p = Pool(jobs)
+    if fastml:
+        check_programs("fasttree", "iqtree")
+    else:
+        check_programs("iqtree")
+
+
+    #tree_results_d = [type_res, alignment_file, tree_gene]
+    tree_results_d = parse_tree_results_file(tree_results_file)
 
     # Ingroup, Gene, Exclude [1]
     # Unknown, Rest of Genes [-] means freely
     # Rest [0]
-
-    t_file = open(tree_results_file,'r')
-
-    tree = {}
-    type = {}
-    alignment = {}
-
-    for line in t_file:
-        line = line.rstrip('\n')
-        line_columns = line.split('\t')
-        type[line_columns[3]] = line_columns[0]
-        alignment[line_columns[3]] = line_columns[1]
-        tree[line_columns[3]] = line_columns[2]
-
-    fasttree_job_list = []
-    iqtree_ml_job_list = []
-    fix_iqtree_list = []
 
     con_tree_list = []
     iqtree_alt_job_list = []
@@ -96,23 +83,26 @@ def main():
 
     if fastml:
         print ("[+] Reconstructing constrainted topologies with FastTree")
+        fasttree_job_list = []
     else:
         print ("[+] Reconstructing constrainted topologies with IQ-TREE")
+        iqtree_ml_job_list = []
+        fix_iqtree_list = []
 
-    for gene in tree:
-        if str(type[gene]) == "HGT":
+    for gene, tree_results_l in tree_results_d.items():
+        type_res, alignment_file, tree_gene = tree_results_l
+        if type_res == "HGT":
 
             selected_genes.append(gene)
 
             ingroup = ["EGP", "Ingroup"]
             ingroup.append(gene)
-            filename = alignment[gene]
 
             ingroup_list = []
             free_list = []
             rest_list = []
 
-            with open_file(filename) as handle:
+            with open_file(alignment_file) as handle:
                 for record in SeqIO.parse(handle,"fasta"):
                     if any(n in record.id for n in ingroup):
                         ingroup_list.append(record.id)
@@ -123,107 +113,120 @@ def main():
             #Set up files
             all_trees_path =  os.path.join(out_path,gene + ".trees")
             con_path = os.path.join(out_path, gene + ".constraint")
-            con = open(con_path,'w')
 
-            FNULL = open(os.devnull, 'w')
+            with open(con_path, 'w', encoding = "UTF-8") as con:
+                if fastml:
+                    # Constaint file for FastTree
+                    total = len(ingroup_list) + len(free_list) + len(rest_list)
+                    con.write(f" {total} 1\n")
+                    for gene_l in ingroup_list:
+                        con.write(f"{gene_l} 1\n")
+                    for gene_l in free_list:
+                        con.write(f"{gene_l} -\n")
+                    for gene_l in rest_list:
+                        con.write(f"{gene_l} 0\n")
+                    # FastTree params
+                    tree_path = os.path.join(out_path,gene + ".fasttree")
+                    fasttree_params = f"{fasttree_model} -constraints {con_path} {alignment_file} > {tree_path}"
+                    t_list=[fasttree_params]
+                    fasttree_job_list.append(t_list)
 
-            if fastml:
-                # Constaint file for FastTree
-                total = len(ingroup_list) + len(free_list) + len(rest_list)
-                con.write(" " + str(total) +" 1\n")
-                for gene_l in ingroup_list:
-                    con.write(gene_l + " 1\n")
-                for gene_l in free_list:
-                    con.write(gene_l + " -\n")
-                for gene_l in rest_list:
-                    con.write(gene_l + " 0\n")
-                con.close()
-                # FastTree params
-                tree_path = os.path.join(out_path,gene + ".fasttree")
-                fasttree_params = fasttree_model + " -constraints " + con_path + " " + alignment[gene] + " > " + tree_path
-                t_list=[fasttree_params]
-                fasttree_job_list.append(t_list)
+                    con_tree_list.append([tree_path,tree_gene,all_trees_path])
+                    #IqTree Alt Params
+                    iqtree_alt_params = f"-quiet -nt {iq_threads} {fastml_iqmodel} -n 0 -zb 10000 -au -pre {tree_path} -s {alignment_file} -z {all_trees_path}"
+                    iqtree_alt_job_list.append(iqtree_alt_params)
 
-                con_tree_list.append([tree_path,tree[gene],all_trees_path])
-                #IqTree Alt Params
-                iqtree_alt_params = "-quiet -nt " + str(iq_threads) + " " + fastml_iqmodel + " -n 0 -zb 10000 -au -pre " + tree_path + " -s " + alignment[gene] + " -z " + all_trees_path
-                iqtree_alt_job_list.append(iqtree_alt_params)
+                else:
+                    # Constaint file for IqTree
+                    ingroup_list = [w.replace('@', '_') for w in ingroup_list]
+                    rest_list = [w.replace('@', '_') for w in rest_list]
+                    con.write(f"(({', '.join(ingroup_list)}),{', '.join(rest_list)});\n")
+                    #IqTree Params
+                    tree_pre = os.path.join(out_path,gene)
+                    tree_path = tree_pre + ".treefile"
+                    iqtree_ml_params = f"-quiet -nt {iq_threads} {iqmodel} -bb {ufbootstrap} -g {con_path} -s {alignment_file} -pre {tree_pre}"
+                    iqtree_ml_job_list.append(iqtree_ml_params)
+                    fix_iqtree_list.append(tree_path)
 
-            else:
-                # Constaint file for IqTree
-                ingroup_list = [w.replace('@', '_') for w in ingroup_list]
-                rest_list = [w.replace('@', '_') for w in rest_list]
-                con.write("((" + ', '.join(ingroup_list) + ")," + "" + ', '.join(rest_list) + ");" + "\n")
-                con.close()
-                #IqTree Params
-                tree_pre = os.path.join(out_path,gene)
-                tree_path = tree_pre + ".treefile"
-                iqtree_ml_params = "-quiet -nt " + str(iq_threads) + " " + iqmodel + " -bb " + str(ufbootstrap) + " -g " + con_path + " -s " + alignment[gene] + " -pre " + tree_pre
-                iqtree_ml_job_list.append(iqtree_ml_params)
-                fix_iqtree_list.append(tree_path)
+                    con_tree_list.append([tree_path,tree_gene,all_trees_path])
+                    #IqTree Alt Params
+                    iqtree_alt_params = f"-quiet -nt {iq_threads} {iqmodel} -n 0 -zb 10000 -au -pre {tree_path} -s {alignment_file} -z {all_trees_path}"
+                    iqtree_alt_job_list.append(iqtree_alt_params)
 
-                con_tree_list.append([tree_path,tree[gene],all_trees_path])
-                #IqTree Alt Params
-                iqtree_alt_params = "-quiet -nt " + str(iq_threads) + " " + iqmodel + " -n 0 -zb 10000 -au -pre " + tree_path + " -s " + alignment[gene] + " -z " + all_trees_path
-                iqtree_alt_job_list.append(iqtree_alt_params)
-
-    if fastml:
-        for i, _ in enumerate(p.imap_unordered(run_fasttree,fasttree_job_list),1):
-            progress(i,1,len(fasttree_job_list))
-    else:
-        for i, _ in enumerate(p.imap_unordered(run_iqtree,iqtree_ml_job_list),1):
-            progress(i,1,len(iqtree_ml_job_list))
-        #Add @ back to the treefile because iqtree removes it
-        for tree in fix_iqtree_list:
-            fix_iqtree(tree)
+    with Pool(jobs) as p:
+        if fastml:
+            for i, _ in enumerate(p.imap_unordered(run_fasttree,fasttree_job_list),1):
+                progress(i,1,len(fasttree_job_list))
+        else:
+            for i, _ in enumerate(p.imap_unordered(run_iqtree,iqtree_ml_job_list),1):
+                progress(i,1,len(iqtree_ml_job_list))
+            #Add @ back to the treefile because iqtree removes it
+            for tree in fix_iqtree_list:
+                fix_iqtree(tree)
 
     for con_tree in con_tree_list:
         concatenate_trees(con_tree)
 
-
+    print ("[+] Calculating stats with IQ-TREE")
     #Add jobs for iqtree
     jobs = get_num_jobs(False, threads, iq_threads)
-    p = Pool(jobs)
+    with Pool(jobs) as p:
 
-    print ("[+] Calculating stats with IQ-TREE")
-
-    for i, _ in enumerate(p.imap_unordered(run_iqtree,iqtree_alt_job_list),1):
-        progress(i,1,len(iqtree_alt_job_list))
+        for i, _ in enumerate(p.imap_unordered(run_iqtree,iqtree_alt_job_list),1):
+            progress(i,1,len(iqtree_alt_job_list))
 
     alt_res_path = os.path.join(output_dir, "alt_topology_results.tsv")
-    alt_res = open(alt_res_path,'w')
-    alt_res.write("#gene"+ "\t" +"deltaL" + "\t" + "p-AU"+ "\t" + "Significantly worse" + "\n")
-    #Create final file
-    for gene in selected_genes:
-
-        if fastml:
-            file = out_path + "/" + gene + ".fasttree.iqtree"
-        else:
-            file = out_path + "/" + gene + ".treefile.iqtree"
-
-        with open(file) as f:
-            content = f.readlines()
-
-        for i, line in enumerate(content):
-            if "USER TREES" in line:
-                tree_1 = content[i+7].split()
-                tree_2 = content[i+8].split()
-        deltaL = float(tree_1[2])-float(tree_2[2])
-        au=tree_1[11]
-        if tree_1[12] == "-":
-            sf=1
-        else:
-            sf =0
-        alt_res.write(gene + "\t" + str(deltaL) + "\t" + str(au) + "\t" + str(sf)+"\n")
-
-    alt_res.close()
-
-
+    write_alt_results(alt_res_path, selected_genes, fastml, out_path)
 
 def concatenate_trees(con_tree_list):
+    """Concatenate the two trees together"""
     tree_path, tree_gene, all_trees_path = con_tree_list
-    FNULL = open(os.devnull, 'w')
-    subprocess.call("cat " + tree_path + " " + tree_gene + " | sed 's/@/_/g' > " + all_trees_path, shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
+    with open(os.devnull, 'w', encoding = "UTF-8") as fn:
+        subprocess.call("cat " + tree_path + " " + tree_gene + " | sed 's/@/_/g' > " + all_trees_path, shell=True, stdout=fn, stderr=subprocess.STDOUT)
+
+def parse_tree_results_file(tree_results_file):
+    """ Parses the tree_results file """
+    tree_results_d = {}
+
+    with open(tree_results_file, 'r', encoding = "UTF-8") as t_file:
+        for line in t_file:
+            line = line.rstrip('\n')
+            line_columns = line.split('\t')
+            if not os.path.exists(line_columns[2]):
+                sys.exit(f"[x] Cannot find tree {line_columns[2]} from {tree_results_file}")
+            gene = line_columns[3]
+            l = [line_columns[0], line_columns[1], line_columns[2]]
+            tree_results_d[gene] = l
+
+    return tree_results_d
+
+def write_alt_results(alt_res_path, selected_genes, fastml, out_path):
+    """ Write the alt results tsv """
+    with open(alt_res_path, 'w', encoding = "UTF-8") as alt_res:
+        alt_res.write("#gene\tdeltaL\tp-AU\tSignificantly worse\n")
+        #Create final file
+        for gene in selected_genes:
+
+            if fastml:
+                file = out_path + "/" + gene + ".fasttree.iqtree"
+            else:
+                file = out_path + "/" + gene + ".treefile.iqtree"
+
+            with open(file, 'r', encoding = "UTF-8") as f:
+                content = f.readlines()
+
+            tree_1 = ""
+            tree_2 = ""
+            for i, line in enumerate(content):
+                if "USER TREES" in line:
+                    tree_1 = content[i+7].split()
+                    tree_2 = content[i+8].split()
+            delta_l = float(tree_1[2])-float(tree_2[2])
+            if tree_1[12] == "-":
+                sf = 1
+            else:
+                sf = 0
+            alt_res.write(f"{gene}\t{delta_l}\t{tree_1[11]}\t{sf}\n")
 
 if __name__ == '__main__':
     main()
